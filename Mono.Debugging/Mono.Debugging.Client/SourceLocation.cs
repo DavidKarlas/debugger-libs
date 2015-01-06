dcfec45e204e393f69f73c23e2674e758d1f97d5
David Karlaš <david.karlas@xamarin.com>
2015-01-06 19:04:24 +0100

    changes needed for alternative file location

--- Mono.Debugging/Mono.Debugging.Client/SourceLocation.cs
+++ Mono.Debugging/Mono.Debugging.Client/SourceLocation.cs
@@ -6,21 +6,23 @@ namespace Mono.Debugging.Client
 	public class SourceLocation
 	{
 		public string MethodName { get; private set; }
-		public string FileName { get; private set; }
+		public string FileName { get; internal set; }
 		public int Line { get; private set; }
 		public int Column { get; private set; }
+		public byte[] FileHash { get; private set; }
 
 		public SourceLocation (string methodName, string fileName, int line)
-			: this (methodName, fileName, line, -1)
+			: this (methodName, fileName, line, -1, null)
 		{
 		}
 
-		public SourceLocation (string methodName, string fileName, int line, int column)
+		public SourceLocation (string methodName, string fileName, int line, int column, byte[] hash = null)
 		{
 			this.MethodName = methodName;
 			this.FileName = fileName;
 			this.Line = line;
 			this.Column = column;
+			this.FileHash = hash;
 		}
 		
 		public override string ToString ()
